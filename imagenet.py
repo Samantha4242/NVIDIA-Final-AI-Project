6210fff8b92a236554defe2f56cd6f5936260dd8
Samantha4242 <samantha@francis-web.com>
2024-08-08 11:05:38 -0700

    initial commit

---
+++ imagenet.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python3
+#
+# Copyright (c) 2020, NVIDIA CORPORATION. All rights reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+#
+
+import requests
+
+import sys
+import argparse
+
+from jetson_inference import imageNet
+from jetson_utils import videoSource, videoOutput, cudaFont, Log
+
+
+def download_file(ID, Name):
+
+    response = requests.get(ID)
+    if response.status_code == 200:
+        with open(f'{Name}.jpg', 'wb') as file:
+            file.write(response.content)
+        print("Downloaded", Name, "successfully")
+    else:
+        print("Failed to download the file")
+        
+        
+print("Hello! I am an AI which has been trained to identify diffrent types of dice for table top role play games!")
+
+# parse the command line
+parser = argparse.ArgumentParser(description="Classify a live camera stream using an image recognition DNN.", 
+                                 formatter_class=argparse.RawTextHelpFormatter, 
+                                 epilog=imageNet.Usage() + videoSource.Usage() + videoOutput.Usage() + Log.Usage())
+
+parser.add_argument("input", type=str, default="", nargs='?', help="URI of the input stream")
+parser.add_argument("output", type=str, default="", nargs='?', help="URI of the output stream")
+#parser.add_argument("--network", type=str, default="googlenet", help="pre-trained model to load (see below for options)")
+#parser.add_argument("--topK", type=int, default=1, help="show the topK number of class predictions (default: 1)")
+
+try:
+	args = parser.parse_known_args()[0]
+except:
+	print("")
+	parser.print_help()
+	sys.exit(0)
+
+
+ID = input("Please enter the image url which you would like to identify: ")
+Name = input("Please enter the name you would like to save the file under: ")
+out_name = Name + '_Output.jpg'
+download_file(ID, Name)
+
+print('HELLO SAM, THIS IS IMAGENET!!!!')
+
+# load the recognition network
+#net = imageNet(args.network, sys.argv)
+
+# note: to hard-code the paths to load a model, the following API can be used:
+#
+net = imageNet(model="models/Dice/resnet18.onnx", labels="models/Dice/labels.txt", 
+                input_blob="input_0", output_blob="output_0")
+
+# create video sources & outputs
+input = videoSource(Name + '.jpg', argv=sys.argv)
+output = videoOutput(out_name, argv=sys.argv)
+font = cudaFont()
+
+# process frames until EOS or the user exits
+while True:
+    # capture the next image
+    img = input.Capture()
+
+    if img is None: # timeout
+        continue  
+
+    # classify the image and get the topK predictions
+    # if you only want the top class, you can simply run:
+    #   class_id, confidence = net.Classify(img)
+    predictions = net.Classify(img, topK=1)
+
+    # draw predicted class labels
+    for n, (classID, confidence) in enumerate(predictions):
+        classLabel = net.GetClassLabel(classID)
+        confidence *= 100.0
+
+        print(f"imagenet:  {confidence:05.2f}% class #{classID} ({classLabel})")
+
+        font.OverlayText(img, text=f"{confidence:05.2f}% {classLabel}", 
+                         x=5, y=5 + n * (font.GetSize() + 5),
+                         color=font.White, background=font.Gray40)
+                         
+    # render the image
+    output.Render(img)
+
+    # update the title bar
+    output.SetStatus("{:s} | Network {:.0f} FPS".format(net.GetNetworkName(), net.GetNetworkFPS()))
+
+    # print out performance info
+    net.PrintProfilerTimes()
+
+    # exit on input/output EOS
+    if not input.IsStreaming() or not output.IsStreaming():
+        break
